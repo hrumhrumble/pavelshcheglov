f5c6ad8d5c41b769ca1a82374b23904e90608f1e
Pavel Shcheglov <p.scheglov@gmail.com>
2015-05-15 08:59:51 +0300

    add sorted by positions

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,2 +1,11 @@
 module ProfileHelper
+
+  def queries_for site
+    site.queries.sort_by { |query| query.positions.last.place }
+  end
+
+  def positions_for query
+    query.positions.map(&:place).reverse
+  end
+
 end
