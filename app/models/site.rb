d1de90cdb40b3c4e6242fe7372ea138bbc0ffee4
Pavel Shcheglov <p.scheglov@gmail.com>
2015-05-17 16:12:04 +0300

    refactor site model

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,12 +2,20 @@ class Site < ActiveRecord::Base
   belongs_to :user
   has_many :queries, dependent: :destroy
 
-  def have_queries_or_positions?
-    self.queries.any? && self.queries.first.positions.any?
+  def have_queries?
+    self.queries.any?
+  end
+
+  def have_positions?
+    self.queries.first.positions.any?
   end
 
   def sorted_queries
-    self.queries.sort_by { |query| query.positions.last.place }
+    if self.have_positions?
+      self.queries.sort_by { |query| query.positions.last.place }
+    else
+      self.queries
+    end
   end
 
   def position_date
