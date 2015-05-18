7329393090e584545eb3157df6566c849956a9aa
Pavel Shcheglov <p.scheglov@gmail.com>
2015-05-18 17:27:08 +0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email: 'p.scheglov@gmail.com', password: '1234')
+User.create!(email: 'demo@demo.com', name: 'Demo', password: '12345678', role: 'user')
+
+puts 'Create some data...'
+3.times do |site_index|
+  User.find_by(email: 'demo@demo.com').sites.create(url: "site#{site_index}.ru")
+  10.times do |query_index|
+    15.times do |day|
+      Site.find_by(url: "site#{site_index}.ru").queries.create(word: "query#{query_index}-#{site_index}")
+      Query.find_by(word: "query#{query_index}-#{site_index}").positions.create(place: rand(1..20), created_at: Date.today - day)
+    end
+  end
+end
+
