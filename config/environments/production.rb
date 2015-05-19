f85dfa474f0236f06ac6e264e117d9a3b51255ba
Pavel Shcheglov <p.scheglov@gmail.com>
2015-05-19 13:30:21 +0300

    fix production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'livefaq.ru', port: 80 }
+  config.action_mailer.default_url_options = { host: 'pavelshcheglov.com', port: 80 }
 end
