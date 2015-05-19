46be4ec97f8ea21a8e74fa0b5816728de8106b71
Pavel Shcheglov <p.scheglov@gmail.com>
2015-05-19 14:03:23 +0300

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
