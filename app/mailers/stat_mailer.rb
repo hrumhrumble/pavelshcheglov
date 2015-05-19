2f31742beb4f50fc94c9a87d30d57fedda558430
Pavel Shcheglov <p.scheglov@gmail.com>
2015-05-19 16:51:35 +0300

    refactor mail task

--- app/mailers/stat_mailer.rb
+++ app/mailers/stat_mailer.rb
@@ -3,9 +3,9 @@ class StatMailer < ApplicationMailer
   default from: "stat@pavelshcheglov.com"
   layout 'mailer'
 
-  def daily_stat_info
+  def daily_stat_info(mail)
     @sites = Site.all
-    mail(to: "p.scheglov@gmail.com", subject: 'Статистика')
+    mail(to: mail, subject: 'Статистика')
   end
 
 end
--- lib/tasks/mail.rake
+++ lib/tasks/mail.rake
@@ -2,7 +2,8 @@ namespace :mail do
 
   desc 'sent test email to p.scheglov@gmail.com'
   task :me => :environment do
-    StatMailer.daily_stat_info.deliver_later
+    mail = "p.scheglov@gmail.com"
+    StatMailer.daily_stat_info(mail).deliver_later
   end
 
 end
