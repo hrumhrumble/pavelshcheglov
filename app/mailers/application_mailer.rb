e8e90fc839efbbd10ac1321fe46ec9235a217e15
Pavel Shcheglov <p.scheglov@gmail.com>
2015-05-21 18:15:45 +0300

    try avoid gmail spam

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,5 @@
+require 'digest/sha2'
 class ApplicationMailer < ActionMailer::Base
-
+  default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@pavelshcheglov.com"
+  default from: "info@pavelshcheglov.com"
 end
--- app/mailers/stat_mailer.rb
+++ app/mailers/stat_mailer.rb
@@ -1,11 +1,10 @@
 class StatMailer < ApplicationMailer
 
-  default from: "info@pavelshcheglov.com"
   layout 'mailer'
 
   def daily_stat_info(mail)
     @sites = Site.all
-    mail(to: mail, subject: 'Статистика')
+    mail(to: mail, subject: 'Статистика ')
   end
 
 end
