201c24eb9c424b0f81e03e47a08f05a4f879a468
Pavel Shcheglov <p.scheglov@gmail.com>
2015-05-21 17:05:51 +0300

    switch smtp to yandex

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,10 +80,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :user_name            => ENV['gmail_username'],
-      :password             => ENV['gmail_password'],
+      :address              => "smtp.yandex.ru",
+      :port                 => 25,
+      :user_name            => ENV['yandex_username'],
+      :password             => ENV['yandex_password'],
       :authentication       => "plain",
       :enable_starttls_auto => true
   }
