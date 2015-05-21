7340ca41882f2a4725d1e2c84a3c2ff04167e0cd
Pavel Shcheglov <p.scheglov@gmail.com>
2015-05-21 17:50:11 +0300

    Revert "contacts on home page"
    This reverts commit 9116f4a2297a489ac607f41b08caf46482f39d9f.

--- spec/features/pages_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-feature 'main page' do
-  it 'can visit' do
-    visit root_path
-    expect(page).to have_text('Павел Щеглов')
-    expect(page).to have_text('+7 (915) 0 300 200')
-  end
-end
